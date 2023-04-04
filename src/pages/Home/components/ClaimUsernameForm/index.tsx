7fcc1db825267cba2d5f4d11610fcaeed44e424e
gabrielviol <gabrielviol@hotmail.com>
2023-04-04 14:12:56 -0300

    add react-hook-form

--- src/pages/Home/components/ClaimUsernameForm/index.tsx
+++ src/pages/Home/components/ClaimUsernameForm/index.tsx
@@ -1,15 +1,34 @@
-import { Button, TextInput } from "@ignite-ui/react";
-import { Form } from "./styles";
-import { ArrowRight } from "phosphor-react";
+import { Button, TextInput } from '@ignite-ui/react'
+import { Form } from './styles'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
-export function ClaimUsernameForm(){
-    return (
-        <Form as="form">
-            <TextInput size='sm' prefix="ignite.com/" placeholder="seu-usuario" />
-            <Button size='sm' type="submit">
-                Reservar
-                <ArrowRight />
-            </Button>
-        </Form>
-    )
+const claimUsernameFormSchema = z.object({
+    username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
+export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    
+  }
+
+  return (
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
+      <Button size="sm" type="submit">
+        Reservar
+        <ArrowRight />
+      </Button>
+    </Form>
+  )
 }
