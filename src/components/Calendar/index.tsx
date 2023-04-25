7599f408696efec300f33063c624dd61456d1f6c
gabrielviol <gabrielviol00@hotmail.com>
2023-04-25 17:49:27 -0300

    navigation in months the calendar

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,20 +1,42 @@
+import dayjs from 'dayjs'
 import { CaretLeft, CaretRight } from "phosphor-react";
 import { CalendarActions, CalendarBody, CalendarContainer, CalendarDay, CalendarHeader, CalendarTitle } from "./styles";
 import { getWeekDays } from '../../utils/get-week-days'
+import { useState } from "react";
 
 export function Calendar() {
+    const [currentDate, setCurrentDate] = useState(() => {
+        return dayjs().set('date', 1)
+    })
+
+    function handlePreviousMonth() {
+        const previousMonthDate = currentDate.subtract(1, 'month')
+
+        setCurrentDate(previousMonthDate)
+    }
+
+    function handleNextMonth() {
+        const previousMonthDate = currentDate.add(1, 'month')
+
+        setCurrentDate(previousMonthDate)
+    }
+
     const shortWeekDays = getWeekDays({ short: true })
+
+    const currentMonth = currentDate.format('MMMM')
+    const currentYear = currentDate.format('YYYY')
+
     return (
         <CalendarContainer>
             <CalendarHeader>
                 <CalendarTitle>
-                    Abril <span>2023</span>
+                    {currentMonth} <span>{currentYear}</span>
                 </CalendarTitle>
                 <CalendarActions>
-                    <button>
+                    <button onClick={handlePreviousMonth} title="Previous month">
                         <CaretLeft />
                     </button>
-                    <button>
+                    <button onClick={handleNextMonth} title="Next month">
                         <CaretRight />
                     </button>
                 </CalendarActions>
