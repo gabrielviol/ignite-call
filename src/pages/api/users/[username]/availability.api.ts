80611dc9238a5d4f1ed5d58825aaffb9861a969b
gabrielviol <gabrielviol00@hotmail.com>
2023-04-28 17:19:53 -0300

    create query with blockedDates

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -64,9 +64,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     })
 
     const availableTimes = possibleTimes.filter((time) => {
-        return !blockedTimes.some(
+        const isTimeBlocked = blockedTimes.some(
             (blockedTime) => blockedTime.date.getHours() === time,
         )
+
+        const isTimeInPast = referenceDate.set('hour', time).isBefore(new Date())
+
+        return !isTimeBlocked && !isTimeInPast
     })
 
     return res.json({ possibleTimes, availableTimes })
