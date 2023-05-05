1d7c098f1c7e9312567866a811b433319f7b1b5f
gabrielviol <gabrielviol00@hotmail.com>
2023-05-05 16:49:25 -0300

    SEO apply

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,37 @@
 import { Heading, Text } from '@ignite-ui/react'
 import Image from 'next/image'
 import { Container, Hero, Preview } from './styles'
+import { NextSeo } from 'next-seo'
 
 import previewImage from '../../assets/preview-image.png'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
-        <ClaimUsernameForm />
-      </Hero>
-      <Preview>
-        <Image
-          src={previewImage}
-          height={400}
-          quality={100}
-          priority
-          alt="Calendário simbolizando aplicação em andamento"
-        />
-      </Preview>
-    </Container>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+          <ClaimUsernameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="Calendário simbolizando aplicação em andamento"
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
--- src/pages/register/update-profile/index.page.tsx
+++ src/pages/register/update-profile/index.page.tsx
@@ -7,10 +7,12 @@ import { Container, Header } from '../styles'
 import { FormAnnotation, ProfileBox } from './styles'
 import { useSession } from 'next-auth/react'
 import { GetServerSideProps } from 'next'
-import { unstable_getServerSession, getServerSession } from 'next-auth'
+import { getServerSession } from 'next-auth'
 import { buildNextAuthOptions } from '@/pages/api/auth/[...nextauth].api'
 import { api } from '@/lib/axios'
 import { useRouter } from 'next/router'
+import { NextSeo } from 'next-seo'
+
 
 const updateProfileSchema = z.object({
     bio: z.string(),
@@ -39,38 +41,44 @@ export default function UpdateProfile() {
     }
 
     return (
-        <Container>
-            <Header>
-                <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
-                <Text>
-                    Precisamos de algumas informações para criar seu perfil! Ah, você pode
-                    editar essas informações depois.
-                </Text>
+        <>
+            <NextSeo
+                title="Atualize seu perfil | Ignite Call "
+                noindex
+            />
+            <Container>
+                <Header>
+                    <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+                    <Text>
+                        Precisamos de algumas informações para criar seu perfil! Ah, você pode
+                        editar essas informações depois.
+                    </Text>
 
-                <MultiStep size={4} currentStep={4} />
-            </Header>
+                    <MultiStep size={4} currentStep={4} />
+                </Header>
 
-            <ProfileBox as="form" onSubmit={handleSubmit(handleUpdateProfile)}>
-                <label>
-                    <Text size="sm">Foto de perfil</Text>
-                    <Avatar src={session.data?.user.avatar_url} />
-                </label>
+                <ProfileBox as="form" onSubmit={handleSubmit(handleUpdateProfile)}>
+                    <label>
+                        <Text size="sm">Foto de perfil</Text>
+                        <Avatar src={session.data?.user.avatar_url} />
+                    </label>
 
-                <label>
-                    <Text size="sm">Sobre você</Text>
-                    <TextArea {...register('bio')} />
-                    <FormAnnotation size='sm'>
-                        Fale um pouco sobre você. Isto será exibido em sua página pessoal.
-                    </FormAnnotation>
+                    <label>
+                        <Text size="sm">Sobre você</Text>
+                        <TextArea {...register('bio')} />
+                        <FormAnnotation size='sm'>
+                            Fale um pouco sobre você. Isto será exibido em sua página pessoal.
+                        </FormAnnotation>
 
-                </label>
+                    </label>
 
-                <Button type="submit" disabled={isSubmitting}>
-                    Finalizar
-                    <ArrowRight />
-                </Button>
-            </ProfileBox>
-        </Container>
+                    <Button type="submit" disabled={isSubmitting}>
+                        Finalizar
+                        <ArrowRight />
+                    </Button>
+                </ProfileBox>
+            </Container>
+        </>
     )
 }
 
